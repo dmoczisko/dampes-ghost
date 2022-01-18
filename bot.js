c927b0eca904194e9a4d673a318b4f48caebbbc4
Dan Moczisko <danny.moczisko@gmail.com>
2022-01-18 12:41:59 -0600

    logging user message

--- bot.js
+++ bot.js
@@ -41,10 +41,10 @@ client.on('messageCreate', (msg) => {
 client.on('messageCreate', (msg) => {
     //Store the user message as a variable
     let userMessage = msg.content;
-    
+
     //check if ! is appended to the message and if so, log and send content
     if (msg.content === '!' + userMessage) {
-        console.log(msg.content)
+        console.log(userMessage)
         msg.channel.send({
             content: "The Dynamic message sent is: " + userMessage | "Sent via " + msg.author.username
         });
