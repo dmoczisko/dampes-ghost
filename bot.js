918bb47c55f6a58b49436afb679d902f8ad8d300
Dan Moczisko <danny.moczisko@gmail.com>
2022-01-18 15:05:40 -0600

    cleanup for now

--- bot.js
+++ bot.js
@@ -9,28 +9,11 @@ client.on('ready', () => {
 
 client.login(process.env.BOT_TOKEN)
 
-const jokes = [
-    'I went to a street where the houses were numbered 8k, 16k, 32k, 64k, 128k, 256k and 512k. It was a trip down Memory Lane.',
-    '“Debugging” is like being the detective in a crime drama where you are also the murderer.',
-    'The best thing about a Boolean is that even if you are wrong, you are only off by a bit.',
-    'A programmer puts two glasses on his bedside table before going to sleep. A full one, in case he gets thirsty, and an empty one, in case he doesn’t.',
-    'If you listen to a UNIX shell, can you hear the C?',
-    'Why do Java programmers have to wear glasses? Because they don’t C#.',
-    'What sits on your shoulder and says “Pieces of 7! Pieces of 7!”? A Parroty Error.',
-    'When Apple employees die, does their life HTML5 in front of their eyes?',
-    'Without requirements or design, programming is the art of adding bugs to an empty text file.',
-    'Before software can be reusable it first has to be usable.',
-    'The best method for accelerating a computer is the one that boosts it by 9.8 m/s2.',
-    'I think Microsoft named .Net so it wouldn’t show up in a Unix directory listing.',
-    'There are two ways to write error-free programs; only the third one works.',
-  ];
-
-
 client.on('messageCreate', (msg) => {
     if (msg.content === '!dadjoke') {
         msg.delete();
         msg.channel.send({
-            content: "Dad Joke of the Day | Sent via " + msg.author.username + "\n" + jokes[Math.floor(Math.random() * jokes.length)],
+            content: "Dad Joke of the Day | Sent via " + msg.author.username,
             files: [
                 "./graves/dadjoke.gif"
             ]
@@ -38,33 +21,29 @@ client.on('messageCreate', (msg) => {
     }
 });
 
-client.on('messageCreate', (msg) => {
-    //prefix variable is "!"
-    const prefix = '!';
 
-    //if message doesn't start with prefix or is from bot, return
-    if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+//below is testing code for prefix - in flight for modularity to commands
+// client.on('messageCreate', (msg) => {
+//     //prefix variable is "!"
+//     const prefix = '!';
 
-    //variables to slice the message and shift to lowercase
-    const args = msg.content.slice(prefix.length).trim().split(/ !/);
-    const command = args.shift().toLowerCase();
+//     //if message doesn't start with prefix or is from bot, return
+//     if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+
+//     //variables to slice the message and shift to lowercase
+//     const args = msg.content.slice(prefix.length).trim().split(/ !/);
+//     const command = args.shift().toLowerCase();
 
 
-    const fullMessage = prefix + command;
-    console.log(fullMessage);
+//     const fullMessage = prefix + command;
+//     console.log(fullMessage);
 
-    if (msg.content === fullMessage) {
-        msg.delete();
-        msg.channel.send({
-            content: "Command: " + fullMessage | "Sent via " + msg.author.username
-        });
-    }
+//     if (msg.content === fullMessage) {
+//         msg.delete();
+//         msg.channel.send({
+//             content: "Command: " + fullMessage | "Sent via " + msg.author.username
+//         });
+//     }
 
-    //check if ! is appended to the message and if so, log and send command text through
-    // if(msg.content.startsWith(prefix + command)) {
-    //     msg.channel.send({
-    //         content: "Command" + command | "Sent via " + msg.author.username
-    //     });
-    // }
 
-});
+// });
